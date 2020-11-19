409c05ce57bd9bae2386bd02615907b2abd43eb9
john <ashamekhi@careandcarers.com>
2020-11-19 19:22:01 +0330

    first commit

---
+++ CB.Client/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using CB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Fallback;
+using Polly.Retry;
+
+namespace CB.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        private string _baseAddress = "http://localhost:5000/home/ten";
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+
+        private static Polly.CircuitBreaker.AsyncCircuitBreakerPolicy<HttpResponseMessage> circuitBreaker =
+            Policy.HandleResult<HttpResponseMessage>(x => !x.IsSuccessStatusCode).Or<HttpRequestException>()
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(10),
+                (a, b) => { Console.WriteLine("Break"); },
+                () => { Console.WriteLine("Reset"); },
+                () => { Console.WriteLine("Half"); });
+        private readonly AsyncFallbackPolicy<HttpResponseMessage> fallbackPolicy;
+
+        public HomeController()
+        {
+            _retryPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                .RetryAsync(2, (a, c) => { /*log*/ });
+
+            fallbackPolicy = Policy.HandleResult<HttpResponseMessage>(result => !result.IsSuccessStatusCode)
+                .Or<BrokenCircuitException>()
+                .FallbackAsync(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                {
+                    Content = new  ObjectContent(typeof(Message),new Message
+                    {
+                        Id=1000,
+                        Text = "default text"
+                    },new JsonMediaTypeFormatter())
+                });
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var result = await fallbackPolicy.ExecuteAsync(()=>_retryPolicy.ExecuteAsync(()=>circuitBreaker.ExecuteAsync(()=>client.GetAsync(_baseAddress))));
+            var str = await result.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<ClientMessage>(str);
+            return Ok(obj);
+        }
+    }
+}
---
+++ CB.Server/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CB.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : ControllerBase
+    {
+        static int Counter = 0;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new Message { Id = 1, Text = "my text" });
+        }
+
+        [HttpGet]
+        public IActionResult Odd()
+        {
+            Counter += 1;
+            if (Counter % 2 == 0)
+            {
+                return Ok(new Message { Id = Counter, Text = "successfull. odd requests" });
+            }
+            else
+            {
+                return BadRequest(new Message { Id = Counter, Text = "failed. even requests" });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Ten()
+        {
+            Console.WriteLine($"counter:{Counter}");
+            Counter += 1;
+            if (Counter % 10 == 0)
+            {
+                return Ok(new Message { Id = Counter, Text = "successfull. 10X requests" });
+            }
+            else
+            {
+                return BadRequest(new Message { Id = Counter, Text = "failed. not 10X requests" });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Long()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            System.Threading.Thread.Sleep(20000);
+            sw.Stop();
+            return Ok(new Message
+            {
+                Id=3,
+                Text = $"success. time: {sw.ElapsedMilliseconds/1000} seconds."
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Ex()
+        {
+            throw new Exception("exception ocurred");            
+        }
+
+    }
+}
